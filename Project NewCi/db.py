1b7cfdbfa5526b3a72e64564905fd5e8be9cb5d2
unknown <NXAZ6SN0031510AA023400@outlook.com>
2024-11-26 16:26:01 +0700

    Abis presentasi

--- Project NewCi/db.py
+++ Project NewCi/db.py
@@ -3,8 +3,8 @@
 import pyodbc
 
 def get_connection():
-    server = "AIR\\SQLEXPRESS"
-    database = "Manpro"
+    server = "LAPTOP-5GNHDDSL\\SQLEXPRESS"
+    database = "NewCi"
     connection = pyodbc.connect(
         f'DRIVER={{ODBC Driver 17 for SQL Server}};'
         f'SERVER={server};'
